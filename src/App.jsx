0cbe42b8398acadd5dfb57f72086c179c30fc425
Md Azharuddin <mdazharuddin155332@gmail.com>
2025-03-14 22:21:19 +0530

    make the udate in the laoder animation

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,6 @@ import Loader from './Components/Home/Loader';
 const Layout = React.lazy(() => import('./Components/Layout/Layout'));
 const Home = React.lazy(() => import('./Pages/Home'));
 
-// Loading component
-const LoadingSpinner = () => (
-  <div className="min-h-screen flex items-center justify-center">
-    <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-amber-500"></div>
-  </div>
-);
 
 function App() {
   return (
